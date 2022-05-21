c8f0efe300521417dd9f5af1e60ce3acdd1a6c6c
Gal Shamir <galshamir8@gmail.com>
2022-05-21 16:11:23 +0300

    initial handling in app action bar title, icon

--- app/src/main/java/com/example/inout/MainActivity.java
+++ app/src/main/java/com/example/inout/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.inout;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setActionBar();
     }
+
+    private void setActionBar() {
+        MaterialToolbar toolBar = findViewById(R.id.app_actionBar);
+        toolBar.setBackgroundColor(0xFF018786);
+        setSupportActionBar(toolBar);
+        toolBar.setTitleCentered(true);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(getCurrentTitle());
+        actionBar.setIcon(R.drawable.app_icon);
+    }
+
+    private String getCurrentTitle() {
+        String suffix =false ? "" : " | Logged in as Gal";
+        return getResources().getString(R.string.app_name) + suffix;
+    }
+
+
 }
