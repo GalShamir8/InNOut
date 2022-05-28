53cf74b109a97f9ced2bb8a0053ca6c8a410298a
Gal Shamir <galshamir8@gmail.com>
2022-05-28 09:30:30 +0300

    add support for backPresed

--- app/src/main/java/com/example/inout/MainActivity.java
+++ app/src/main/java/com/example/inout/MainActivity.java
@@ -55,4 +55,15 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
