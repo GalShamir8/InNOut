c0b6b85a20a6510dc945d2ede99f0b4bbbc3394e
Gal Shamir <galshamir8@gmail.com>
2022-05-28 18:36:44 +0300

    add calendar pick Date callback

--- app/src/main/java/com/example/inout/fragments/CalendarFragment.java
+++ app/src/main/java/com/example/inout/fragments/CalendarFragment.java
@@ -5,14 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.inout.R;
+import com.example.inout.common.Callable;
 
 public class CalendarFragment extends Fragment {
 
+    public void setOnDatePickCallback(Callable onDatePickCallback) {
+        this.onDatePickCallback = onDatePickCallback;
+    }
 
+    private Callable onDatePickCallback;
     public CalendarFragment() { }
 
     @Override
@@ -23,6 +29,13 @@ public class CalendarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.calendar_fragment, container, false);
+        View view = inflater.inflate(R.layout.calendar_fragment, container, false);
+        CalendarView calendar = view.findViewById(R.id.calendarView);
+        calendar.setOnDateChangeListener((calendarView, year, month, day) -> {
+            if (onDatePickCallback != null){
+                onDatePickCallback.call(year, month, day);
+            }
+        });
+        return view;
     }
 }
