215543b613ea135f83d095d984e9c8354dca5754
Gal Shamir <galshamir8@gmail.com>
2022-06-25 17:11:58 +0300

    add const values for database integrations

--- app/src/main/java/com/example/inout/utils/MyFirebase.java
+++ app/src/main/java/com/example/inout/utils/MyFirebase.java
@@ -4,7 +4,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MyFirebase {
-
+    private static final String DATABASE_URL = "https://innout-acc3b-default-rtdb.firebaseio.com";
+    private static final String START_HOUR_KEY = "startHour";
+    private static final String START_MINUTE_KEY = "startMin";
+    private static final String END_HOUR_KEY = "endHour";
+    private static final String END_MINUTE_KEY = "endMin";
+    private static final String USERS_PATH = "users";
     private static MyFirebase firebaseInstance = null;
     FirebaseDatabase db;
     private MyFirebase() {
