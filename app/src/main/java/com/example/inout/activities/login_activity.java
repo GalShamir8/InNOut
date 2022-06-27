06bd28b7c76941cc3bb3771e430f5ee76c0ef10c
Gal Shamir <galshamir8@gmail.com>
2022-06-27 20:11:51 +0300

    refactor the code to center fire base concerns in one place

--- app/src/main/java/com/example/inout/activities/login_activity.java
+++ app/src/main/java/com/example/inout/activities/login_activity.java
@@ -5,22 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.example.inout.R;
+import com.example.inout.utils.MyFirebase;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class login_activity extends AppCompatActivity {
-    private FirebaseUser currentUser;
-    private FirebaseAuth mAuth;
 
     private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
             new FirebaseAuthUIActivityResultContract(),
@@ -30,7 +27,6 @@ public class login_activity extends AppCompatActivity {
     private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
         IdpResponse response = result.getIdpResponse();
         if (result.getResultCode() == RESULT_OK) {
-            currentUser = mAuth.getCurrentUser();
             openApp();
         }
     }
@@ -40,15 +36,12 @@ public class login_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
+        if(MyFirebase.getInstance().getUser() != null){
             openApp();
         }
         else{
