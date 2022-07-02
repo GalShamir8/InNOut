46c22b359b3be6bfb323e8e2652e57ba54552639
Gal Shamir <galshamir8@gmail.com>
2022-07-02 12:42:18 +0300

    fix design

--- app/src/main/java/com/example/inout/fragments/ReportFragment.java
+++ app/src/main/java/com/example/inout/fragments/ReportFragment.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.inout.R;
 import com.example.inout.utils.Helper;
@@ -23,6 +24,11 @@ import java.util.HashMap;
 public class ReportFragment extends Fragment {
     private View view;
     private PieChart report_pieChart;
+    private TextView report_TXT_reported;
+    private TextView report_TXT_left;
+    private TextView report_TXT_missing;
+    private TextView report_TXT_total;
+    private TextView report_TXT_title;
 
 
     public ReportFragment() { }
@@ -42,12 +48,18 @@ public class ReportFragment extends Fragment {
 
     private void setViews() {
         report_pieChart = view.findViewById(R.id.report_pieChart);
+        report_TXT_reported = view.findViewById(R.id.report_TXT_reported);
+        report_TXT_left = view.findViewById(R.id.report_TXT_left);
+        report_TXT_missing = view.findViewById(R.id.report_TXT_missing);
+        report_TXT_total = view.findViewById(R.id.report_TXT_total);
+        report_TXT_title = view.findViewById(R.id.report_TXT_title);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void setData() {
         HashMap<String, Integer> reportData = Helper.getInstance().getReportData(
                 Calendar.getInstance().get(Calendar.MONTH) + 1); // +1 for index 0 offset
+        setLabelsData(reportData);
         int leftDays = reportData.get(Helper.getInstance().getLeftDaysKey());
         int reportedDays = reportData.get(Helper.getInstance().getReportedDaysKey());
         int missingReportDays = reportData.get(Helper.getInstance().getMissingDaysKey());
@@ -58,5 +70,17 @@ public class ReportFragment extends Fragment {
         report_pieChart.addPieSlice(new PieModel(
                 getString(R.string.missing_report),missingReportDays , Color.RED));
         report_pieChart.startAnimation();
+    }
+
+    private void setLabelsData(HashMap<String, Integer> reportData) {
+        int leftDays = reportData.get(Helper.getInstance().getLeftDaysKey());
+        int reportedDays = reportData.get(Helper.getInstance().getReportedDaysKey());
+        int missingReportDays = reportData.get(Helper.getInstance().getMissingDaysKey());
+        int totalDaysInMonth = reportData.get(Helper.getInstance().getDaysMonthKey());
+        report_TXT_reported.setText(getString(R.string.reported) + " "  + reportedDays);
+        report_TXT_left.setText(getString(R.string.left) + " " + leftDays);
+        report_TXT_missing.setText(getString(R.string.missing_report) + " " +  missingReportDays);
+        report_TXT_total.setText(totalDaysInMonth + " " + getString(R.string.days_in_month));
+        report_TXT_title.setText(getString(R.string.month_report));
     }
 }
